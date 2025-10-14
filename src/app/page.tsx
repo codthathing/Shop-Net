01c6e421b8a7a6e46ec350445d8d1c1b2c5febdb
Akinwunmi <akinwunmiolusegun277@gmail.com>
2025-10-14 12:14:34 +0100

    bug: updated the text color of the welcome page h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <section className="bg-white h-full flex items-center justify-center">
       <div className="flex flex-col items-center gap-y-4 lg:gap-y-8">
-        <h1 className="text-2xl lg:text-5xl font-extrabold">Welcome to Shop Net</h1>
+        <h1 className="text-2xl lg:text-5xl font-extrabold text-black">Welcome to Shop Net</h1>
         <Link href={"/sneakers"} className="bg-[#ff7d1aff] text-white text-base lg:text-base px-4 py-2 lg:px-5 lg:py-2.5 rounded-md shadow-lg shadow-black/25">Get Started</Link>
       </div>
     </section>
--- src/components/cart/CartSection.tsx
+++ src/components/cart/CartSection.tsx
@@ -8,7 +8,7 @@ export default function CartSection() {
   return (
     <div className={`${showCartSection ? "block" : "hidden"} absolute top-full max-lg:left-0 right-0 mx-auto lg:right-0 bg-white w-[97%] lg:w-80 shadow-xl shadow-black/25 rounded-md mt-2 lg:mt-1.5 z-10`}>
       <header className="p-4 border-b-2 border-[#f7f8fdff]">
-        <span className="font-bold text-base lg:text-lg">Cart</span>
+        <span className="font-bold text-base lg:text-lg text-black">Cart</span>
       </header>
       <CartItemTemplateMain />
     </div>
